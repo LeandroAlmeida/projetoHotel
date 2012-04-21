fb2f397c14992cf9d36dc9639427cfa66688d978
Leandro Almeida <leandro.almeida@dce.ufpb.br>
2012-04-20 21:10:09 -0300

    Classe Gerente Funcionário versão 2, ela controla os funcionários no sistema.

--- src/recepção/GerenteFuncionario.java
+++ src/recepção/GerenteFuncionario.java
@@ -1,24 +1,63 @@
 package recepção;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class GerenteFuncionario {
-	private LinkedList<Funcionario> listaFuncionario;
-	
-	public GerenteFuncionario(){
-		listaFuncionario=new LinkedList<Funcionario>();			
+
+	public LinkedList<Funcionario> listaFuncionarios;
+
+	public GerenteFuncionario() {
+
+		listaFuncionarios = new LinkedList<Funcionario>();
+
 	}
-	
-	public void addFuncionario(Funcionario f){
-		listaFuncionario.add(f);
+
+	public void cadastroFuncionario(Funcionario f) {
+		for(Funcionario fun:this.listaFuncionarios){
+			if(fun.getCpf().equals(f.getCpf()))
+				throw new Excecao("Cpf ja cadastrado!!!");
+		}
+		listaFuncionarios.add(f);
+
 	}
-	
-	public Funcionario consultaFuncionario(String cpf){
-		for(Funcionario f: this.listaFuncionario){
-			if(f.getCpf().equals(cpf))
-				return f;
+
+	public Funcionario removerFuncionario(String cpf) {
+		Funcionario funRemovido;
+		for (Funcionario f : this.listaFuncionarios) {
+			if (f.getCpf().equals(cpf)) {
+				listaFuncionarios.remove(f);
+				funRemovido = f;
+				return funRemovido;
 			}
-		return null;//depois exception
-		
+		}
+		throw new Excecao("Funcionario não existente");
 	}
-}
+
+	public Funcionario consultarFuncionario(String cpf) {
+		
+		for (Funcionario f : this.listaFuncionarios) {
+			if (f.getCpf().equals(cpf))
+				return f;
+		}
+		throw new Excecao("Funcionario não existente");
+
+	}
+
+	public List<Funcionario> informaListaFuncionario() {
+		return listaFuncionarios;
+
+	}
+
+	public Funcionario alterarDadosFuncionario(Funcionario fun){
+			for (Funcionario f : this.listaFuncionarios) {
+					if(fun.getCpf().equals(f.getCpf())){
+						f=fun;
+					return f;
+					}
+				}
+			throw new Excecao("Funcionario não existente");
+			}
+				
+	}
+	
