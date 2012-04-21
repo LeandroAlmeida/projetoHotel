aa3cc08c90093ad49e3e46f04c842e6daa5e55cc
Leandro Almeida <leandro.almeida@dce.ufpb.br>
2012-04-20 21:05:34 -0300

    Classe Estadia, é uma super-classe para as classes Hospedagem e Reserva.

---
+++ src/recepção/Estadia.java
@@ -0,0 +1,84 @@
+package recepção;
+
+import java.util.Date;
+
+public abstract class Estadia {
+	private Date dataEntrada;
+	private Date dataSaida;
+	private Quarto quarto;
+	private Hospede hospede;
+	public Hospede getHospede() {
+		return hospede;
+	}
+	public void setHospede(Hospede hospede) {
+		this.hospede = hospede;
+	}
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+	public void setDataEntrada(Date dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+	public void setDataSaida(Date dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+	public Quarto getQuarto() {
+		return quarto;
+	}
+	public void setQuarto(Quarto quarto) {
+		this.quarto = quarto;
+	}
+	
+	public abstract String getTipoEstadia();
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((dataEntrada == null) ? 0 : dataEntrada.hashCode());
+		result = prime * result
+				+ ((dataSaida == null) ? 0 : dataSaida.hashCode());
+		result = prime * result + ((hospede == null) ? 0 : hospede.hashCode());
+		result = prime * result + ((quarto == null) ? 0 : quarto.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estadia other = (Estadia) obj;
+		if (dataEntrada == null) {
+			if (other.dataEntrada != null)
+				return false;
+		} else if (!dataEntrada.equals(other.dataEntrada))
+			return false;
+		if (dataSaida == null) {
+			if (other.dataSaida != null)
+				return false;
+		} else if (!dataSaida.equals(other.dataSaida))
+			return false;
+		if (hospede == null) {
+			if (other.hospede != null)
+				return false;
+		} else if (!hospede.equals(other.hospede))
+			return false;
+		if (quarto == null) {
+			if (other.quarto != null)
+				return false;
+		} else if (!quarto.equals(other.quarto))
+			return false;
+		return true;
+	}
+	
+
+	
+	
+	
+}
