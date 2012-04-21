8d12091fb3a66c1f26b343922edf3f61e9feb1eb
Leandro Almeida <leandro.almeida@dce.ufpb.br>
2012-04-20 21:18:20 -0300

    Classe Pessoa, classe que contém os dados de uma pessoa cadastrada no sistema.

--- src/recepção/Pessoa.java
+++ src/recepção/Pessoa.java
@@ -4,10 +4,28 @@ import java.util.Date;
 
 public class Pessoa {
 	private String nome;
-	private Date dataNacimento;
+	private String dataNacimento;
 	private String cpf;
 	private String rg;
+	private String Tel;
+	private String email;
 	
+	public String getTel() {
+		return Tel;
+	}
+
+	public void setTel(String tel) {
+		Tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public Pessoa(){
 		this.nome="sem nome";
 		this.dataNacimento=null;
@@ -23,11 +41,11 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-	public Date getDataNacimento() {
+	public String getDataNacimento() {
 		return dataNacimento;
 	}
 
-	public void setDataNacimento(Date dataNacimento) {
+	public void setDataNacimento(String dataNacimento) {
 		this.dataNacimento = dataNacimento;
 	}
 
@@ -49,4 +67,61 @@ public class Pessoa {
 	public String toString(){
 		return "nome é: "+this.nome+"\ndata de nascimento: "+this.dataNacimento+"\ncpf: "+this.cpf+"\nrg: "+this.rg+"\nprofição: ";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Tel == null) ? 0 : Tel.hashCode());
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result
+				+ ((dataNacimento == null) ? 0 : dataNacimento.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (Tel == null) {
+			if (other.Tel != null)
+				return false;
+		} else if (!Tel.equals(other.Tel))
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (dataNacimento == null) {
+			if (other.dataNacimento != null)
+				return false;
+		} else if (!dataNacimento.equals(other.dataNacimento))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
+		return true;
+	}
+	
 }
