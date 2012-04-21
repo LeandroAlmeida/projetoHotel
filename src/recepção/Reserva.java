5f3771d3150cd4c02dc1b4aba54229bfce69b48d
Leandro Almeida <leandro.almeida@dce.ufpb.br>
2012-04-20 21:21:29 -0300

    Classe Reserva, classe que contém as informações referentes a uma reserva.

--- src/recepção/Reserva.java
+++ src/recepção/Reserva.java
@@ -1,5 +1,43 @@
 package recepção;
 
-public class Reserva {
+import java.util.Date;
 
+public class Reserva extends Estadia{
+	
+	boolean Confirmacao;
+	
+	
+	Reserva(){
+		Confirmacao=false;
+	}
+
+	public void setConfirmacao() {
+		Confirmacao = true;
+	}
+	
+	
+	public String getTipoEstadia(){
+		return "Reserva";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (Confirmacao ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reserva other = (Reserva) obj;
+		if (Confirmacao != other.Confirmacao)
+			return false;
+		return true;
+	}
 }
