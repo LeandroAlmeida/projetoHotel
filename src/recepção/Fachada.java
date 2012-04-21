782b0f1ff6d602d7e0f7ad9669108dd25e997ef7
Leandro Almeida <leandro.almeida@dce.ufpb.br>
2012-04-20 21:07:48 -0300

    Classe Fachada versão 2, com as funcionalidades do programa.

--- src/recepção/Fachada.java
+++ src/recepção/Fachada.java
@@ -1,51 +1,53 @@
 package recepção;
 
+import java.text.ParseException;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Fachada {
 	private GerenteHospede gerenteHospede;
 	private GerenteFuncionario gerenteFuncionario;
-	private GerenteHotel gerenteHotel;
-	private GerenteHospedagem gerenteHospegem;
+	private GerenteHotelQuarto gerenteHotel;
+	private GerenteHospedagem gerenteHospedagem;
 	
 	public Fachada(){
 		this.gerenteHospede = new GerenteHospede();
 		this.gerenteFuncionario=new GerenteFuncionario();
-		this.gerenteHotel=new GerenteHotel();
-		this.gerenteHospegem= new GerenteHospedagem();
+		this.gerenteHotel=new GerenteHotelQuarto();
+		this.gerenteHospedagem= new GerenteHospedagem();
 	}
 	
 	/*
 	 * Responsabilidade gerenteFuncionario
-	 * Cadastra um funcionário no sistema.
+	 * Cadastrar um funcionário no sistema.
 	 */
 	public void cadastroFuncionario(Funcionario f){
-		gerenteFuncionario.addFuncionario(f);
+		gerenteFuncionario.cadastroFuncionario(f);
 	}
 	
 	/*
 	 * Responsabilidade gerenteFuncionario
-	 * Remove um funcionário do sistema.
+	 * Remover um funcionário do sistema.
 	 */
 	public Funcionario removerFuncionario(String cpf){
-		return null;
+		return gerenteFuncionario.removerFuncionario(cpf);
 	}
 	
 	/*
 	 * Responsabilidade gerenteFuncionario
-	 * Consulta os dados de um funcionário.
+	 * Consultar os dados de um funcionário.
 	 */
 	public Funcionario consultarFuncionario(String cpf){
-		return gerenteFuncionario.consultaFuncionario(cpf);
+		return gerenteFuncionario.consultarFuncionario(cpf);
 	}
 	
 	/*
 	 * Responsabilidade gerenteFuncionario
-	 * Informa uma lista com os funcionários atuais, mostrando nome, cpf, função.
+	 * Informar uma lista com os funcionários atuais, mostrando nome, cpf, função.
 	 */
 	public List<Funcionario> informaListaFuncionario(){
-		return null;
+		return gerenteFuncionario.informaListaFuncionario();
 	}
 	
 	
@@ -53,8 +55,8 @@ public class Fachada {
 	 * Responsabilidade gerenteFuncionario
 	 * Alterar dados de um funcionário, procura esse funcionário(com o mesmo cpf) na lista e sobrescreve com f.
 	 */
-	public void alterarDadosFuncionario(Funcionario f){
-		
+	public Funcionario alterarDadosFuncionario(Funcionario f){
+		return gerenteFuncionario.alterarDadosFuncionario(f);
 	}
 	
 	/*
@@ -62,7 +64,7 @@ public class Fachada {
 	 * Cadastra um hospede no sistema.
 	 */
 	public void cadastroHospede(Hospede h){
-		
+		gerenteHospede.cadastrarHospede(h);
 	}
 	
 	/*
@@ -70,7 +72,7 @@ public class Fachada {
 	 * Remove um hospede do sistema.
 	 */
 	public Hospede removerHospede(String cpf){
-		return null;
+		return gerenteHospede.removerHospede(cpf);
 	}
 	
 	/*
@@ -78,15 +80,22 @@ public class Fachada {
 	 * Altera os dados do hospede cadastrado no sistema, procura esse hospede(com o mesmo cpf) na lista e sobrescreve com h.
 	 */
 	public void alterarDadosHospede(Hospede h){
-		
+		gerenteHospede.alterarDadosHospede(h);
 	}
 
 	/*
 	 * Responsabilidade gerenteHospede
-	 * Consulta histórico de hospedagem de um hospede.
+	 * Retorna o hospede com esse cpf.
 	 */
 	public Hospede consultarHospede(String cpf){
-		return null;
+		return gerenteHospede.consultarHospede(cpf);
+	}
+	
+	/*
+	 * Retorna uma lista de hospedagem realizada por um hospede
+	 */
+	public List<Hospedagem> consultarHistorico(String cpf){
+		return gerenteHospede.consultarHistorico(cpf);
 	}
 	
 	/*
@@ -94,7 +103,7 @@ public class Fachada {
 	 * Cadastra um quarto no sistema.
 	 */
 	public void cadastraQuarto(Quarto q){
-		
+		gerenteHotel.cadastraQuarto(q);
 	}
 	
 	/*
@@ -102,23 +111,38 @@ public class Fachada {
 	 * remover um quarto no sistema.
 	 */
 	public Quarto removerQuarto(int numero){
-		return null;
+		return gerenteHotel.removerQuarto(numero);
 	}
 	
 	/*
 	 * Responsabilidade gerenteHotel
-	 * Altera a discrição de um quarto no sistema
+	 * Altera a descrição de um tipo de quarto no sistema, exemplo: altera a descrição de todos quartos do tipo básico.
 	 */
 	public void alterarDescricaoQuarto(String descrição, String tipoDeQuarto){
-		
+		gerenteHotel.alterarDescricaoQuarto(descrição, tipoDeQuarto);
 	}
 	
 	/*
 	 * Responsabilidade gerenteHotel
-	 * Altera o preço de um quarto.
+	 * Altera o preço de um tipo de quarto no sistem,  exemplo: altera a preço de todos quartos do tipo básico.
 	 */
-	public void alterarPrecoQuarto(String tipoDeQuarto, double preco){
-		
+	public void alterarPrecoQuarto(double preco,String tipoDeQuarto){
+		gerenteHotel.alterarPrecoQuarto(tipoDeQuarto, preco);
+	}
+	
+	/*
+	 * Responsabilidade gerenteHotel
+	 * Retorna as informações do quarto de um determinado número
+	 */
+	public Quarto consultarInformacaoQuarto(int num){
+		return gerenteHotel.getQuarto(num);
+	}
+	/*
+	 * Responsabilidade gerenteHotel
+	 * Retorna uma lista de quarto(s) atual do sistema.
+	 */
+	public List<Quarto> listaDeQuartoAtual(){
+		return gerenteHotel.listaDeQuartoAtual();
 	}
 	
 	/*
@@ -126,23 +150,49 @@ public class Fachada {
 	 * Realiza uma reserva de um quarto no sistema.
 	 */
 	public void realizarReserva(Reserva r){
-		
+		gerenteHospedagem.realizarReserva(r);
+	}
+	
+	/*
+	 * Cancelar uma reserva no sistema
+	 */
+	public Reserva cancelarReserva(String Cpf){
+		return gerenteHospedagem.cancelarReserva(Cpf);
+	}
+	/*
+	 * Confirma uma reserva no sistema, ao confirmar os dados dessa reserva passa para uma hospedagem e ela e removida
+	 */
+	public void confirmaReserva(String Cpf){
+		gerenteHospedagem.confirmarReserva(Cpf);
+	}
+	/*
+	 * Consulta uma reserva no sistema
+	 */
+	public Reserva consultaReserva(String cpf){
+		return gerenteHospedagem.consultarReserva(cpf);
 	}
 	
 	/*
 	 * Responsabilidade gerenteHospedagem
-	 * Realiza hospedagem!
+	 * Realizar hospedagem!
 	 */
 	public void realizarHospedagem(Hospedagem h){
-		
+		 gerenteHospedagem.realizarHospegagem(h);
+	}
+	
+	/*
+	 * Consultar uma hospedagem no sistema
+	 */
+	public Hospedagem consultaHospedagem(String cpf){
+		return gerenteHospedagem.consultarHospedagem(cpf);
 	}
 	
 	/*
 	 * Responsabilidade gerenteHospedagem
-	 * Confere se um quarto esta vago.
+	 * Conferir se um quarto esta vago.
 	 */
 	public boolean isQuartoVago(int numero, Date dataEntrada, Date dataSaida){
-		return true;
+		return gerenteHospedagem.isQuartoVago(numero,dataEntrada,dataSaida);
 	}
 	
 	
@@ -150,16 +200,21 @@ public class Fachada {
 	 * Responsabilidade gerenteHospedagem
 	 * Fecha conta total ou parcial.
 	 */
-	public double fechaConta(int numero, boolean fechaContaTotal){
-		return 0.0;
+	public double fechaConta(String cpf, boolean fechaContaTotal) throws ParseException{
+		return gerenteHospedagem.fechaConta(cpf, fechaContaTotal);
 	}
 	/*
 	 * Responsabilidade gerenteHospedagem
-	 * Informa uma lista com todas as hospedagens de um determinado dia, informando quarto e o nome do hospede.
+	 * Informa uma lista com todas as reservas de um dia ou dias anteriores, podendo assim, remover essas reservas que não se 
+	 * tornaram hospedagem.
 	 */
-	public List<Hospedagem> informaListaHospedagemDia(Date dia){
-		return null;
+	public LinkedList<Reserva> informaListaReservasPendentes(Date dia){
+		return gerenteHospedagem.informaListaDeReservasPendentes(dia);
 	}
 	
+	
+	public void adicionarConsumo(int numero, Consumo c){
+		gerenteHospedagem.addConsumoHospedagem(numero,c);
+	}
 }
 
